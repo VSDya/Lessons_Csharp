914c373ac136f8b5960f1fd36089b5ed33d0af5f
VSDya <vdyadyanin@yandex.ru>
2023-03-11 19:49:38 +0300

    Add Methods for in for

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -77,6 +77,13 @@ string Method4(int count, string text)
 Console.WriteLine(res);
 
 
-
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine(); // разрыв таблицы или переход
+}
 
 
