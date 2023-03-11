d180ce02582ca4f45ff1f96eeaec0d367e98b1a6
VSDya <vdyadyanin@yandex.ru>
2023-03-11 19:38:50 +0300

    Add Methods for

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -51,20 +51,29 @@ Console.WriteLine(year);
 
 
 // Вид4 (что то принимают и что то возвращают)
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+string res = Method4(10, "new method ");
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = string.Empty;
-
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
-
-string res = Method4(10, "new method ");
 Console.WriteLine(res);
 
 
