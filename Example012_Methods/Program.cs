eb7a90d8423ebe7eaca9d449ca5de23eaf1f42cf
VSDya <vdyadyanin@yandex.ru>
2023-03-12 20:27:01 +0300

    Add Methods Array

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -109,7 +109,7 @@ string Replace(string text, char oldValue, char newValue)
     int length = text.Length;
     for (int i = 0; i < length; i++)
     {
-        if(text[i] == oldValue) result = result + $"{newValue}";
+        if (text[i] == oldValue) result = result + $"{newValue}";
         else result = result + $"{text[i]}";
     }
 
@@ -124,7 +124,63 @@ newText = Replace(newText, 'к', 'К');
 Console.WriteLine(newText);
 
 
+// Массив
 
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void SelectionSort1(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+
+
+PrintArray(arr);
+SelectionSort(arr);
+SelectionSort1(arr);
+PrintArray(arr);
 
 
 
